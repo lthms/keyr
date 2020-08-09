3057892d2d81b61670b8dafe9088d0fe1a9f2c39
Thomas Letan <lthms@soap.coffee>
2020-08-09 14:55:37 +0200

    Refactor `mul' to use structures and impl

--- muf/main.rs
+++ muf/main.rs
@@ -1,15 +1,16 @@
 use chrono::Utc;
-use std::io::{Result, Read, Write, Seek, SeekFrom};
+use std::io::{Result, Read};
 use std::os::unix::net::UnixStream;
-use std::fs::{OpenOptions, File};
-use std::path::Path;
+use std::fs::OpenOptions;
 
-fn open_data_file(path : &Path) -> Result<File> {
+use mul::{CounterFile, DayFile, GlobalFile, EntryLoc};
+
+fn open_data_options() -> OpenOptions {
     OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
-        .open(path)
+        .clone()
 }
 
 fn muu_fetch() -> Result<u32> {
@@ -22,54 +23,41 @@ fn muu_fetch() -> Result<u32> {
 }
 
 fn update_global_count(count : u32) -> Result<u32> {
-    let mut global_file = open_data_file(&mul::data_path("counter")?)?;
+    let mut global = GlobalFile::open(open_data_options())?;
 
-    let prev = mul::read_count(&mut global_file)
-        .unwrap_or(0u32);
+    let prev = global.read_global_count().unwrap_or(0u32);
 
     let new_count = count + prev;
 
-    global_file.seek(SeekFrom::Start(0))?;
-    mul::write_count(&mut global_file, new_count)?;
+    global.seek_global_count()?;
+    global.write_global_count(new_count)?;
 
     Ok(new_count)
 }
 
-fn update_hourly_count(count : u32) -> Result<u32> {
+fn update_day_count(count : u32) -> Result<u32> {
     let date = Utc::now();
-
-    let mut today_file = open_data_file(
-        &mul::data_path(&date.format("%Y%m%d").to_string())?
-    )?;
-
     let cur_key = date.format("%H%M").to_string();
 
-    let global_count = mul::read_count(&mut today_file)
+    let mut today_file = DayFile::open(&date.date(), open_data_options())?;
+
+    let global_count = today_file.read_global_count()
         .map(|x| x + count)
         .unwrap_or(count);
 
-    today_file.seek(SeekFrom::Start(0))?;
+    today_file.seek_global_count()?;
+    today_file.write_global_count(global_count)?;
 
-    mul::write_count(&mut today_file, global_count)?;
-
-    today_file.write_all(&global_count.to_le_bytes())?;
-
-    if let Some((pre_key, pre_count)) =
-        mul::read_last_entry(&mut today_file)? {
+    if let Some((pre_key, pre_count)) = today_file.read_entry(EntryLoc::Last)? {
         if pre_key == cur_key {
             let new_count = pre_count + count;
 
-            today_file.seek(SeekFrom::End(-4))?;
-            mul::write_count(&mut today_file, new_count)?;
+            today_file.update_count(new_count, EntryLoc::Previous)?;
         } else {
-            mul::write_key(&mut today_file, &cur_key)?;
-            mul::write_count(&mut today_file, count)?;
+            today_file.add_entry(cur_key, count)?;
         }
     } else {
-        today_file.seek(SeekFrom::Start(4))?;
-        mul::write_key(&mut today_file, &cur_key)?;
-        mul::write_count(&mut today_file, count)?;
-
+        today_file.add_entry(cur_key, count)?;
     }
 
     Ok(global_count)
@@ -79,7 +67,7 @@ fn main() -> Result<()> {
     let count = muu_fetch()?;
 
     let new_global_count = update_global_count(count)?;
-    let today_count = update_hourly_count(count)?;
+    let today_count = update_day_count(count)?;
 
     println!("{} ({} today)", new_global_count, today_count);
 
--- mul/lib.rs
+++ mul/lib.rs
@@ -1,50 +1,184 @@
-use std::fs::File;
-use std::path::PathBuf;
+use chrono::{Date, Utc};
+use chrono::offset::TimeZone;
+use std::fs::{OpenOptions, File};
+use std::path::{Path, PathBuf};
 use std::io::{Result, Read, Write, Seek, SeekFrom};
 
-pub fn data_path(name : &str) -> Result<PathBuf> {
+pub trait CounterFile {
+    fn seek_global_count(&mut self) -> Result<()>;
+    fn read_global_count(&mut self) -> Result<u32>;
+    fn write_global_count(&mut self, count : u32) -> Result<()>;
+}
+
+impl CounterFile for File {
+    fn seek_global_count(&mut self) -> Result<()> {
+        self.seek(SeekFrom::Start(0))?;
+
+        Ok(())
+    }
+
+    fn read_global_count(&mut self) -> Result<u32> {
+        let mut buf = [0u8; 4];
+        self.read_exact(&mut buf)?;
+
+        Ok(u32::from_le_bytes(buf))
+    }
+
+    fn write_global_count(&mut self, count : u32) -> Result<()> {
+        self.write_all(&count.to_le_bytes())
+    }
+}
+
+pub struct GlobalFile(File);
+
+impl GlobalFile {
+    pub fn open(opts : OpenOptions) -> Result<GlobalFile> {
+        Ok(GlobalFile(opts.open(data_path("counter")?)?))
+    }
+}
+
+impl CounterFile for GlobalFile {
+    fn seek_global_count(&mut self) -> Result<()> {
+        self.0.seek_global_count()
+    }
+
+    fn read_global_count(&mut self) -> Result<u32> {
+        self.0.read_global_count()
+    }
+
+    fn write_global_count(&mut self, count : u32) -> Result<()> {
+        self.0.write_global_count(count)
+    }
+}
+
+pub enum EntryLoc {
+    First,
+    Next,
+    Previous,
+    Last,
+}
+
+impl EntryLoc {
+    pub fn to_seek_from(&self) -> SeekFrom {
+        match self {
+            EntryLoc::First => SeekFrom::Start(4),
+            EntryLoc::Next => SeekFrom::Current(0),
+            EntryLoc::Previous => SeekFrom::Current(-8),
+            EntryLoc::Last => SeekFrom::End(-8)
+        }
+    }
+}
+
+pub struct DayFile(File);
+
+impl CounterFile for DayFile {
+    fn seek_global_count(&mut self) -> Result<()> {
+        self.0.seek_global_count()
+    }
+
+    fn read_global_count(&mut self) -> Result<u32> {
+        self.0.read_global_count()
+    }
+
+    fn write_global_count(&mut self, count : u32) -> Result<()> {
+        self.0.write_global_count(count)
+    }
+}
+
+impl DayFile {
+    pub fn open(date : &Date<Utc>, opts : OpenOptions) -> Result<DayFile> {
+        let path = data_path(&date.format("%Y%m%d").to_string())?;
+
+        Ok(DayFile(opts.open(path)?))
+    }
+
+    fn read_key(&mut self) -> Result<String> {
+        let mut buf = [0u8; 4];
+        self.0.read_exact(&mut buf)?;
+
+        // FIXME
+        Ok(String::from_utf8(Vec::from(buf.as_ref())).unwrap())
+    }
+
+    fn read_count(&mut self) -> Result<u32> {
+        let mut buf = [0u8; 4];
+        self.0.read_exact(&mut buf)?;
+
+        Ok(u32::from_le_bytes(buf))
+    }
+
+    pub fn read_entry(&mut self, entry : EntryLoc) -> Result<Option<(String, u32)>> {
+        match self.0.seek(entry.to_seek_from())
+            .and_then(|_| self.read_key()) {
+            Ok(key) => { // we have been able to read a key
+                         // if we cannot read a count, there is an error in our
+                         // file
+                Ok(Some((key, self.read_count()?)))
+            }
+            _ => { // we could not read a key, so we assume there is no
+                   // entry to read
+                Ok(None)
+            },
+        }
+    }
+
+    pub fn add_entry(&mut self, key : String, count : u32) -> Result<()> {
+        self.0.write_all(key.as_bytes())?;
+        self.0.write_all(&count.to_le_bytes())
+    }
+
+    pub fn update_count(&mut self, count : u32, entry : EntryLoc) -> Result<()> {
+        self.0.seek(entry.to_seek_from())?;
+        self.0.seek(SeekFrom::Current(4))?;
+
+        self.0.write_all(&count.to_le_bytes())
+    }
+}
+
+fn data_path(name : &str) -> Result<PathBuf> {
     let xdg_dirs = xdg::BaseDirectories::with_prefix("muu").unwrap();
 
     xdg_dirs.place_config_file(name)
 }
 
-pub fn read_count(file : &mut File) -> Result<u32> {
-    let mut buf = [0u8; 4];
-    file.read_exact(&mut buf)?;
+fn parse_filename(p : &Path) -> Option<Date<Utc>> {
+    let re_file = regex::Regex::new(r"^(\d{4})(\d{2})(\d{2})$").unwrap();
 
-    Ok(u32::from_le_bytes(buf))
+    let filename = p.file_name()?.to_string_lossy();
+
+    let cap = re_file.captures_iter(&filename).next()?;
+
+    let year = cap[1].parse::<i32>().ok()?;
+    let month = cap[2].parse::<u32>().ok()?;
+    let day = cap[3].parse::<u32>().ok()?;
+
+    Utc.ymd_opt(year as i32, month, day).single()
 }
 
-pub fn write_count(file : &mut File, count : u32) -> Result<()> {
-    file.write_all(&count.to_le_bytes())?;
+pub fn parse_key(key : &str) -> Option<(u32, u32)> {
+    let re_file = regex::Regex::new(r"^(\d{2})(\d{2})$").unwrap();
 
-    Ok(())
+    let cap = re_file.captures_iter(key).next()?;
+
+    let hour = cap[1].parse::<u32>().ok()?;
+    let minute = cap[2].parse::<u32>().ok()?;
+
+    Some((hour, minute))
 }
 
-pub fn read_key(file : &mut File) -> Result<String> {
-    let mut buf = [0u8; 4];
-    file.read_exact(&mut buf)?;
+pub fn list_days() -> Result<Vec<Date<Utc>>> {
+    let xdg_dirs = xdg::BaseDirectories::with_prefix("muu").unwrap();
+    let mut res = vec![];
 
-    // FIXME
-    Ok(String::from_utf8(Vec::from(buf.as_ref())).unwrap())
-}
+    for candidate in xdg_dirs.get_config_home().read_dir()? {
+        let candidate = candidate?.path();
 
-pub fn write_key(file : &mut File, key : &str) -> Result<()> {
-    file.write_all(key.as_bytes())?;
-
-    Ok(())
-}
-
-pub fn read_last_entry(file : &mut File) -> Result<Option<(String, u32)>> {
-    match file.seek(SeekFrom::End(-8)) {
-        Ok(_) => {
-            let key = read_key(file)?;
-            let count = read_count(file)?;
-
-            Ok(Some((key, count)))
-        },
-        _ => {
-            Ok(None)
+        if candidate.is_file() {
+            if let Some(x) = parse_filename(&candidate) {
+                res.push(x);
+            }
         }
     }
+
+    Ok(res)
 }
